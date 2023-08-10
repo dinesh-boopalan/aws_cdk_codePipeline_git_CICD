16c7b7916d7d4c107cc40a1b6a0fc40911b2709f
dineshboopalan <dinesh.boopalan@genesys.com>
2023-08-10 17:06:39 +0530

    cicd

--- bin/aws_cdk_codepipeline_git_cicd.ts
+++ bin/aws_cdk_codepipeline_git_cicd.ts
@@ -4,18 +4,6 @@ import * as cdk from 'aws-cdk-lib';
 import { AwsCdkCodepipelineGitCicdStack } from '../lib/aws_cdk_codepipeline_git_cicd-stack';
 
 const app = new cdk.App();
-new AwsCdkCodepipelineGitCicdStack(app, 'AwsCdkCodepipelineGitCicdStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new AwsCdkCodepipelineGitCicdStack(app, 'DemoawspipelineStack', {
+  env: { account: '951845455890', region: 'us-east-1' },
 });
--- lib/aws_cdk_codepipeline_git_cicd-stack.ts
+++ lib/aws_cdk_codepipeline_git_cicd-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkCodepipelineGitCicdStack extends cdk.Stack {
@@ -7,10 +9,19 @@ export class AwsCdkCodepipelineGitCicdStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkCodepipelineGitCicdQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // AWS CI-CD Pipeline
+    const democicdpipeline = new CodePipeline(this,'demopipeline',
+{
+      synth: new ShellStep('Synth', {
+        // Use a connection created using the AWS console to authenticate to GitHub
+        // Other sources are available.
+        input: CodePipelineSource.gitHub('dinesh-boopalan/aws_cdk_codepipeline_git_cicd', 'main'),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth',
+        ],
+      }),
+    });
   }
 }
